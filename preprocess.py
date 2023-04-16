d86b13b15639f93595adbed601d2b2d71eab19b1
salmanFarshi <33355278+sftSalman@users.noreply.github.com>
2023-04-16 23:17:25 +0600

    Delete preprocess.py

--- preprocess.py
+++
@@ -1,6 +0,0 @@
-# Set the paths to the image folder and annotation folder
-image_folder = image_sourch
-annotation_folder = annotation_sourch
-
-# Call the preprocessing function
-preprocessing(image_folder, annotation_folder)
